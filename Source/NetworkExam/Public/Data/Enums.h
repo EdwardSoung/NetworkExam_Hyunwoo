c2e2c7cf61401cac5ba60a14ef251bfb4c6611de
hyunwoo <allegoria88@gmail.com>
2026-01-16 18:00:14 +0900

    과제 기본 생성 코드 및 서버맵 생성

---
+++ Source/NetworkExam/Public/Data/Enums.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+UENUM()
+enum class EGameState : uint8
+{
+	Lobby,
+	Waiting,
+	Playing,
+	Finished,
+};
